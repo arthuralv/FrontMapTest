b0637a44011776a70146bf3edc2fa3dc186bf4eb
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2020-07-04 21:03:02 -0300

    Novo filtro

--- activity.py
+++ activity.py
@@ -1,5 +1,6 @@
 import csv
-import unicodedata
+from unicodedata import normalize
+from math import ceil
 from difflib import SequenceMatcher
 
 def ehSemelhante(str1, str2):
@@ -15,7 +16,7 @@ def main():
 
         for row in dbReader:
             bairro = row['bairro'].lower()
-            bairro = unicodedata.normalize('NFKD', bairro).encode('ASCII','ignore').decode('ASCII').rstrip()
+            bairro = normalize('NFKD', bairro).encode('ASCII','ignore').decode('ASCII').rstrip()
             if bairro in bairros:
                 bairros[bairro] += 1
             else:
@@ -23,7 +24,7 @@ def main():
         
         for row in fileReader:
             bairro = row['Bairro'].lower()
-            bairro = unicodedata.normalize('NFKD', bairro).encode('ASCII','ignore').decode('ASCII').rstrip()
+            bairro = normalize('NFKD', bairro).encode('ASCII','ignore').decode('ASCII').rstrip()
             if bairro in bairros_csv:
                 bairros_csv[bairro] += 1
             else:
@@ -35,13 +36,19 @@ def main():
                     bairros[bairro] += bairros_csv[bairro_csv]
         
     with open('cases_cg.csv', 'r', encoding='utf8') as reference, open('cases_cg_filtered.csv', 'w', newline='\n', encoding='utf8') as db:
-        dbWriter = csv.DictWriter(db, dbReader.fieldnames)
+        dbWriter = csv.DictWriter(db, dbReader.fieldnames + ['n/1000'])
         dbWriter.writeheader()
         refReader = csv.DictReader(reference)
 
         for row in refReader:
-            dbWriter.writerow({'bairro': row['bairro'], 'cases': bairros[unicodedata.normalize('NFKD', row['bairro'].lower()).encode('ASCII','ignore').decode('ASCII').rstrip()]})
+            casos = bairros[normalize('NFKD', row['bairro'].lower()).encode('ASCII','ignore').decode('ASCII').rstrip()]
 
+            dbWriter.writerow({'bairro': row['bairro'], 'cases': casos, 'n/1000': formatar(casos/1000)})
+
+def formatar(num = 0):
+    inteiro, flutuante = str(num).split('.')
+    flutuante = str(ceil(float(flutuante[:3]) / 10) * 10)
+    return float(inteiro + '.' + str(flutuante))
 
 main()
 
