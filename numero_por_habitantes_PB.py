e42470391ad247efb56e3e8c0da2b00b0938c3ed
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2020-07-13 11:07:09 -0300

    Update

--- numero_por_habitantes_PB.py
+++ numero_por_habitantes_PB.py
@@ -20,22 +20,23 @@ def main():
             if row['state'] == 'PB':
                 if row['city'] != 'CASO SEM LOCALIZAÇÃO DEFINIDA/PB':
                     city = row['city'][:-3].upper()
-                    cases = row['totalCases']
+                    cases = int(row['totalCases'])
                     
-                    n_1000 = formatar(int(cases), find(city))
+                    n_1000 = float(formatar(int(cases), find(city)))
                     soma += int(cases)
                     
-                    lista.append((int(cases), city))
-                    hab.append((float(n_1000), city))
+                    lista.append((cases, n_1000, city))
+                    hab.append((n_1000, cases, city))
                     
                     fileWriter.writerow({'city': city, 'Ncasos': cases, 'Ncasos/1000H': n_1000})
-
         lista.sort()
         hab.sort()
+
     print('\nMAIORES NÚMEROS DE CASOS------')
     show(lista[-5:])
     print('\nMAIORES NÚMEROS POR 1000 HAB--')
-    show(hab[-5:])
+    show(hab[-5:], 'hab')
+    print('\n\nTOTAL DE CASOS:', soma)
 
 def formatar(num = 0, popCity = 1):
     num = (1000 * num) / popCity
@@ -52,8 +53,21 @@ def find(city):
                 return int(row['pop'])
         return 1
 
-def show(list):
+def show(list, type='std'):
+    text1 = ''
+    text2 = ''
+
+    if type == 'std':
+        text2 = 'por 1,000 habitantes'
+    else:
+        text1 = 'por 1,000 habitantes'
+    # Exemplo = Patos: 1,306 / 12.14 por 1,000 habitantes
+    
+    
     for elem in list:
-        print(str(elem[1]) + ':', elem[0])
+        print(str(elem[2]).capitalize() + ':', text1, decimalFormat(elem[0]), '/', decimalFormat(elem[1]), text2)
+    
+def decimalFormat(num):
+    return '{:,}'.format(num)
 
 main()
