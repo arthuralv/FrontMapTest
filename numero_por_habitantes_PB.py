baf741d9b041c19f0b4624d24c481f4ebdca8a7d
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2020-07-13 14:13:29 -0300

    Update

--- numero_por_habitantes_PB.py
+++ numero_por_habitantes_PB.py
@@ -31,12 +31,8 @@ def main():
                     fileWriter.writerow({'city': city, 'Ncasos': cases, 'Ncasos/1000H': n_1000})
         lista.sort()
         hab.sort()
-
-    print('\nMAIORES NÚMEROS DE CASOS------')
-    show(lista[-5:])
-    print('\nMAIORES NÚMEROS POR 1000 HAB--')
-    show(hab[-5:], 'hab')
-    print('\n\nTOTAL DE CASOS:', soma)
+    
+    showLegend(lista[-5:], hab[-5:], thousand_format(soma))
 
 def formatar(num = 0, popCity = 1):
     num = (1000 * num) / popCity
@@ -53,21 +49,62 @@ def find(city):
                 return int(row['pop'])
         return 1
 
-def show(list, type='std'):
-    text1 = ''
-    text2 = ''
-
-    if type == 'std':
-        text2 = 'por 1,000 habitantes'
+def show_cases(case_list):
+    for elem in case_list:
+            print(capitalize(elem[2]) + ':', thousand_format(elem[0]), '/', decimal_format(elem[1]), 'por 1000 habitantes')
     else:
-        text1 = 'por 1,000 habitantes'
+        text1 = 'por 1.000 habitantes'
     # Exemplo = Patos: 1,306 / 12.14 por 1,000 habitantes
+
+def show_cases_per_hab(case_list):
+    for elem in case_list:
+            print(capitalize(elem[2]) + ':', decimal_format(elem[0]), 'por 1000 habitantes', '/', thousand_format(elem[1]))
     
+def thousand_format(num):
+    return f'{num:,}'.replace(',','.')
+
+def decimal_format(num):
+    return f'{num}'.replace('.',',')
+
+def showLegend(lista, hab, total):
     
-    for elem in list:
-        print(str(elem[2]).capitalize() + ':', text1, decimalFormat(elem[0]), '/', decimalFormat(elem[1]), text2)
+    days = '13'
+    mouths = '07'
+    years = '20'
+
+    hours = '11'
+    minutes = '00'
+
+    cidade_MQdC = capitalize(lista[-1][2])
+    casos_MQdC = thousand_format(lista[-1][0])
+
+    cidade_MQdC_PH = capitalize(hab[-1][2])
+    casos_MQdC_PH = decimal_format(hab[-1][0])
+
+    print(
+f'''A foto acima é um mapa de calor (Heat Map) que mostra a quantidade de casos confirmados de COVID-19 por 1000 habitantes, de acordo com cada região da Paraíba até o dia {days}/{mouths}/{years} às {hours}:{minutes} hora(s), no total de {total} casos.
+
+A intensidade das cores representam a escala de quantidade de casos por 1000 habitantes em cada cidade.
+
+Atualmente a área com maior número de casos na Paraíba é de {casos_MQdC}, em {cidade_MQdC}. Entretanto, a cidade com maior número de casos por 1000 habitantes é {cidade_MQdC_PH} com {casos_MQdC_PH} aproximadamente.
+'''
+        )
     
-def decimalFormat(num):
-    return '{:,}'.format(num)
+    print('As 5 cidades com maior quantidade de casos:')
+    show_cases(lista)
+    
+    print('\n')
+
+    print('As 5 cidades com maior quantidade de casos por 1000 habitantes:')
+    show_cases_per_hab(hab)
+
+def capitalize(phrase):
+    phrase_list = phrase.split()
+    new = phrase_list[0].capitalize()
+
+    for i in range(1, len(phrase_list)):
+        new += ' ' + phrase_list[i].capitalize() if len(phrase_list[i]) > 2 else ' ' + phrase_list[i].lower()
+
+    return new
 
 main()
